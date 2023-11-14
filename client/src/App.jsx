5a4e4c5687a50c5ae5114dec09d665de9dea30a3
Jitendra <jitendra@pop-os.localdomain>
2023-11-14 17:37:50 +0530

    first fix

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+import { Footer } from './Components/Footer';
+import { Navbar } from './Components/Navbar'
+import { Routes } from './Components/Routes';
+
+const App = () => {
+  const [darkTheme, setDarkTheme] = useState(false);
+
+  return (
+    <div className={darkTheme ? 'dark' : ''}>
+      <div className="dark:bg-gray-900 bg-gray-100 dark:text-gray-200 black min-h-screen">
+        <Navbar setDarkTheme={setDarkTheme} darkTheme={darkTheme} />
+        <Routes />
+        <Footer />
+      </div>
+    </div>
+
+  );
+};
+
+export default App;
